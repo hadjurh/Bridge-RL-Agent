58a7e40c7d2c0cc18a9140ed72cf81add54c1307
hugo.h <hugo.hadjur@grenoble-inp.org>
2018-07-06 12:03:18 +0900

    time correction

--- utils/memory.py
+++ utils/memory.py
@@ -5,14 +5,16 @@ import datetime
 def write_file(step, set_size, observation_list, action_list, score_list, unique_id):
     print("Step: " + str(step), file=sys.stderr)
 
+    now = datetime.datetime.now()
+    time_str = str(now)[0:10] + "_" + str(now)[11:19].replace(":", "-")
+
     file_game = open("database/no_trump_" + str(set_size) + "_" +
-                     str(datetime.datetime.now())[0:10] + "_" +
-                     str(datetime.datetime.now())[11:19].replace(":", "-") + "_" + unique_id +
+                     time_str + "_" + unique_id +
                      ".game", "w")
     file_score = open("database/no_trump_" + str(set_size) + "_" +
-                      str(datetime.datetime.now())[0:10] + "_" +
-                      str(datetime.datetime.now())[11:19].replace(":", "-") + "_" + unique_id +
+                      time_str + "_" + unique_id +
                       ".score", "w")
+
     file_game.write(''.join((str(observation) + "\n" + str(action) + "\n")
                             for observation, action in zip(observation_list, action_list)))
     file_score.write(''.join((str(score) + "\n")
