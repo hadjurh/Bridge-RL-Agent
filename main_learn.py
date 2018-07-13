dc625193ee85faabf4293da15d51a944a26684d4
hugo.h <hugo.hadjur@grenoble-inp.org>
2018-07-13 11:25:22 +0900

    delete game files

--- main_learn.py
+++ main_learn.py
@@ -59,5 +59,5 @@ if __name__ == '__main__':
 
     print(round(time.time() - start, 3), "sec.", file=sys.stderr)
 
-    # os.remove(str(file))
-    # os.remove("database/" + file_name_no_extension + ".score")
+    os.remove(str(file))
+    os.remove("database/" + file_name_no_extension + ".score")
